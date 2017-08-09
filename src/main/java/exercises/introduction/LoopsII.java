26d9fcfe991bfac50a2197e81c1a52d66aeb432b
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-08-09 17:13:40 -0300

    Add Java Loops II exercise.

---
+++ src/main/java/exercises/introduction/LoopsII.java
@@ -0,0 +1,33 @@
+package exercises.introduction;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+import static java.lang.Math.pow;
+import static java.lang.System.in;
+import static java.lang.System.out;
+
+public class LoopsII {
+    public void printSequenceNumber() {
+        Scanner scanner = new Scanner(in);
+
+        int queries = scanner.nextInt();
+
+        IntStream.range(0, queries).forEach(i -> {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+
+            IntStream.range(0, n)
+                    .boxed()
+                    .reduce(a, (accumulator, current) -> {
+                        final int sum = accumulator + (int)pow(2, current) * b;
+
+                        out.format(current == 0 ? "%s" : " %s", sum);
+                        return sum;
+                    });
+
+            out.println();
+        });
+    }
+}
---
+++ src/test/java/exercises/introduction/LoopsIITest.java
@@ -0,0 +1,51 @@
+package exercises.introduction;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+import static java.lang.System.setIn;
+import static java.lang.System.setOut;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LoopsIITest {
+    private static final String INPUT =
+            "2\n" +
+            "0 2 10\n" +
+            "5 3 5\n";
+
+    private static final String OUTPUT =
+            "2 6 14 30 62 126 254 510 1022 2046\n" +
+            "8 14 26 50 98\n";
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayInputStream inContent = new ByteArrayInputStream(INPUT.getBytes());
+
+    @Before
+    public void setUp() {
+        setOut(new PrintStream(outContent));
+        setIn(inContent);
+    }
+
+    @Test
+    public void shouldPrintMultiplesForInput() {
+        LoopsII exercise = new LoopsII();
+
+        exercise.printSequenceNumber();
+
+        assertThat(outContent.toString()).isEqualTo(OUTPUT);
+    }
+
+    @After
+    public void tearDown() {
+        setOut(out);
+        setIn(in);
+    }
+
+}
