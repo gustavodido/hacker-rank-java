5fc5c5b2cfb1243efcd9e11ca422468a2a0f8dab
Gu1f3f3 <gustavo_di_domenico@gap.com>
2017-08-15 19:02:16 -0300

    Add Hashset exercise.

---
+++ src/main/java/exercises/datastructures/JavaHashset.java
@@ -0,0 +1,33 @@
+package exercises.datastructures;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class JavaHashset {
+
+    public void solve() {
+        // Given code
+
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        // Custom code
+        
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + pair_right[i]);
+            out.println(set.size());
+        }
+    }
+
+}
---
+++ src/test/java/exercises/datastructures/JavaHashsetTest.java
@@ -0,0 +1,54 @@
+package exercises.datastructures;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static java.lang.System.in;
+import static java.lang.System.out;
+import static java.lang.System.setIn;
+import static java.lang.System.setOut;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JavaHashsetTest {
+    private static final String INPUT = "5\n" +
+            "john tom\n" +
+            "john mary\n" +
+            "john tom\n" +
+            "mary anna\n" +
+            "mary anna\n";
+
+    private static final String OUTPUT = "1\n" +
+            "2\n" +
+            "2\n" +
+            "3\n" +
+            "3\n";
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayInputStream inContent = new ByteArrayInputStream(INPUT.getBytes());
+
+    @Before
+    public void setUp() {
+        setOut(new PrintStream(outContent));
+        setIn(inContent);
+    }
+
+    @Test
+    public void shouldSolveForFirstScenario() {
+        JavaHashset exercise = new JavaHashset();
+
+        exercise.solve();
+
+        assertThat(outContent.toString()).isEqualTo(OUTPUT);
+    }
+
+    @After
+    public void tearDown() {
+        setOut(out);
+        setIn(in);
+    }
+}
